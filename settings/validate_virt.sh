ba4e8a948f85e6e83cabf45f03e2bb0677c83eec
ejko36 <102948820+ejko36@users.noreply.github.com>
2022-11-10 16:02:03 +0900

    Update validate_virt.sh

--- settings/validate_virt.sh
+++ settings/validate_virt.sh
@@ -1,1 +1,5 @@
 virt-host-validate
+touch /boot/cmdline.txt
+echo -n "cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" >> /boot/cmdline.txt
+cat /boot/cmdline.txt
+echo "Need reboot 'sudo reboot'"
